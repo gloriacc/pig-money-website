10c5578baf7d69257af538e1ef9d5b61e1b89c45
gloriacc <xuan_1213@yeah.net>
2020-06-27 10:25:48 +0800

    deploy

---
+++ precache-manifest.94f457ad83b8cc083d9aa410d6713cfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e781ec6b37a1ff7dbf9655870087671",
+    "url": "/pig-money-website/index.html"
+  },
+  {
+    "revision": "79f3b783ae760458365e",
+    "url": "/pig-money-website/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "7e9598d04329464b4a95",
+    "url": "/pig-money-website/static/css/main.4dcc7583.chunk.css"
+  },
+  {
+    "revision": "79f3b783ae760458365e",
+    "url": "/pig-money-website/static/js/2.db26f8fb.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/pig-money-website/static/js/2.db26f8fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9598d04329464b4a95",
+    "url": "/pig-money-website/static/js/main.38241122.chunk.js"
+  },
+  {
+    "revision": "b82bc5fdfebbf87ae5d1",
+    "url": "/pig-money-website/static/js/runtime-main.a7357a01.js"
+  }
+]);
